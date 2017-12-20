d15f541d804ae8f5d9df1b706763fbbdcda32359
Vaibhav T. Indalkar <vindalkar@xento.com>
2017-12-20 17:04:42 +0530

    core data entity added

---
+++ SwiftTraning/ToDoListExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListExtension.swift
+//  SwiftTraning
+//
+//  Created by Vaibhav T. Indalkar on 20/12/17.
+//  Copyright © 2017 Vaibhav Indalkar. All rights reserved.
+//
+
+import CoreData
+
+extension ToDoList {
+    
+    func saveTask(coreDataObject:ToDoList,context:NSManagedObjectContext) {
+        
+    }
+}
